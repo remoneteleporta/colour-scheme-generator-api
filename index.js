60bca43500d7f7fc2584be868c7a2bfb1cee10c5
Remone Teleporta <remoneteleporta@gmail.com>
2025-11-16 19:34:29 +0530

    css for seed colour input bar added, seed colour and scheme selection bar set in html

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-fetch("https://www.thecolorapi.com/scheme")
+const colourPick = document.getElementById("colour-picker")
+const seedhexInput = document.getElementById("seed-colour-hex")
+
+fetch("https://www.thecolorapi.com/scheme?hex=24B1E0&mode=triad&count=6")
 .then(resp => resp.json())
 .then(data => console.log(data))
